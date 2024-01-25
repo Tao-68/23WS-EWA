a3f1a9e69e434cd2885e62fb57d382790536c430
Rohit <rohit.kuinkel@stud.h-da.de>
2024-01-25 19:08:13 +0100

    prak 4

--- src/Praktikum/Prak4/frontend/bestellung.js
+++ src/Praktikum/Prak4/frontend/bestellung.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const pizzaPriceElement = document.querySelector(`input[name="singlePizzaPrice"][value="${option.title}"]`);
       if (pizzaPriceElement) 
       {
-        const multiplier = parseInt(option.text.split(' ')[1].substring(1)); // the name is smth like Salami x2 so we take the 2nd part and remove the x
+        const multiplier = parseInt(option.text.split(' ')[1].substring(1)); // the name looks like "Salami x2" so we take the 2nd part and remove the x
         const pizzaPrice = pizzaPriceElement.value;
         total = total + pizzaPrice * multiplier;
       }
@@ -35,11 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   pizzaSelect.addEventListener('change', updateTotalPrice);
   
-  //When a user submits a form, it triggers the forms submit event which is not only triggered by clicking the submitOrder button but also by other means such as 
-  //pressing the Enter key when this form is in focus, so by attaching the event listener to the forms submit event and not the submitOrder button itself, we ensure that 
-  //the validation logic is executed consistently whenever the form is submitted regardless of the means by which it is submitted
   pizzaOrderForm.addEventListener('submit', () => {
     if (submitOrder.disabled || pizzaSelect.options.length === 0 || addressInput.value.length === 0) 
+      //console.log('Please select at least 1 Pizza and provide a valid address to place an order.');
       alert('Please select at least 1 Pizza and provide a valid address to place an order.');   
   });
   
@@ -84,26 +82,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
     
-
-  //if the user clicks on an image, search for the corresponding pizza with the same name as the image in the select element and then add it to the cart
-  //problem: cant add the same element multiple times to the cart
-  //solution: selected the div as a whole so we got more info about the pizza like its price,name etc.
   const pizzaDivs = document.querySelectorAll('div[id="pizzaImages"]');
   pizzaDivs.forEach((pizzaDiv) => {
     const pizzaImage = pizzaDiv.querySelector('img');
     pizzaImage.addEventListener('click', () => {
       const pizzaName = pizzaImage.alt;
       const existingOption = Array.from(pizzaSelect.options).find(option => option.title === pizzaImage.getAttribute('data-price'));
-      //console.log(existingOption);
       if (existingOption) 
       {
-        //console.log(existingOption.title);
         pizzaDict[existingOption.title]= pizzaDict[existingOption.title]+1;
         existingOption.text = `${pizzaName} x${pizzaDict[existingOption.title]}`;
       } 
       else 
       {
-        //console.log('in new');
         const newOption = document.createElement('option');
         newOption.value = pizzaDiv.getAttribute('data-value');
         newOption.text = pizzaName + ' x1';
@@ -126,51 +117,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// function contrastGesamtPreisWithBackgroundColor()
-//   {
-//     const gesamtPreisElement = document.getElementById('gesamtPreis');
-//     const bodyBackgroundColor = document.body.style.backgroundColor;
-//     const isBackgroundColorLight = isColorLight(bodyBackgroundColor);
-//     console.log("Body Background Color:", bodyBackgroundColor);
-//     console.log("Is Background Color Light:", isBackgroundColorLight);
-//     gesamtPreisElement.style.color = isBackgroundColorLight ? "#000000" : "#FFFFFF";
-//   }
-    
-//   function isColorLight(color) 
-//   {
-//     var rgb = getColorRgb(color);
-//     //console.log("rgb:", rgb);
-//     var r = (rgb >> 16) & 0xff;
-//    // console.log("r:", r);
-//     var g = (rgb >> 8) & 0xff;
-//     //console.log("g:", g);
-//     var b = rgb & 0xff; // No need to shift here
-//     //console.log("b:", b);
-//     var average = (r + g + b) / 3;
-//     //console.log("avg:", average);
-//     return average > 100;
-//   }
-  
-//   function getColorRgb(color) 
-//   {
-//     if (color.startsWith("rgb")) 
-//     {
-//       const rgbArray = color.match(/\d+/g);
-//       return (parseInt(rgbArray[0]) << 16) | (parseInt(rgbArray[1]) << 8) | parseInt(rgbArray[2]);
-//     } 
-//     else 
-//     {
-//       const tempDiv = document.createElement("div");
-//       tempDiv.style.color = color;
-//       document.body.appendChild(tempDiv);
-//       const rgb = getComputedStyle(tempDiv).color.match(/\d+/g);
-//       document.body.removeChild(tempDiv);
-//       return (parseInt(rgb[0]) << 16) | (parseInt(rgb[1]) << 8) | parseInt(rgb[2]);
-//     }
-//   }
-
-// window.onload = () => {
-//   contrastGesamtPreisWithBackgroundColor();
-//   window.setInterval(contrastGesamtPreisWithBackgroundColor, 1000);
-// }
   
